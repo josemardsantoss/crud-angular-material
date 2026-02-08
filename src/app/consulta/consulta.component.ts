5e59680045102cdd5785c8901b454999120d781f
Josemar dos Santos <jjlbms2@gmail.com>
2026-02-07 21:02:35 -0300

    Populando a lista de clientes.

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input'
-import {MatCardModule} from '@angular/material/card'
-import {MatIconModule} from '@angular/material/icon'
-import {MatTableModule} from '@angular/material/table'
-import {FormsModule} from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatTableModule } from '@angular/material/table'
+import { FormsModule } from '@angular/forms'
 import { MatButton } from "@angular/material/button";
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
@@ -12,6 +14,15 @@ import { MatButton } from "@angular/material/button";
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
+
+  listaClientes: Cliente[] = []
+
+  constructor(private service: ClienteService) {
+
+  }
+  ngOnInit() {
+    this.listaClientes = this.service.pesquisarClientes('')
+  }
 
 }
