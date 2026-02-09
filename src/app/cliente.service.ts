7fed7714033c8daedfab3c4c3dcde5b4504cb2cb
Josemar dos Santos <jjlbms2@gmail.com>
2026-02-09 20:56:47 -0300

    Implementando lógica para deletar o registro.

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -41,6 +41,13 @@ export class ClienteService {
     })
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage))
   }
+  deletar(cliente: Cliente) {
+    const storage = this.obterStorage();
+    const novaLista = storage.filter(c => c.id !== cliente.id);
+
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(novaLista))
+
+  }
 
   private obterStorage(): Cliente[] {
     const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
